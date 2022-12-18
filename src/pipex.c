ae102978c00c57ca748a92cfdb374b46b48d3bfb
Clément Paluszek <105011036+Cpaluszek@users.noreply.github.com>
2022-12-18 11:35:12 +0100

    fork function setup to execute command

--- src/pipex.c
+++ src/pipex.c
@@ -6,17 +6,18 @@
 /*   By: cpalusze <cpalusze@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/14 12:48:00 by cpalusze          #+#    #+#             */
-/*   Updated: 2022/12/18 11:17:36 by cpalusze         ###   ########.fr       */
+/*   Updated: 2022/12/18 11:34:44 by cpalusze         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 #include <stdlib.h>
 
+void	execute_program(char *prog_path, char **env);
+
 int	main(int argc, char **argv, char **env)
 {
 	int		file1;
-	int		exec_ret;
 	char	*prog_path;
 
 	if (argc != 5)
@@ -25,8 +26,29 @@ int	main(int argc, char **argv, char **env)
 	if (file1 == -1)
 		file_error();
 	prog_path = find_program(argv[2], env);
-	exec_ret = execve(prog_path, argv + 2, env);
+	execute_program(prog_path, env);
 	free(prog_path);
-	if (exec_ret == -1)
-		program_error();
+	printf("Parent process exit\n");
+}
+
+// Creating a child process to execute a program
+// Note: search about waitpid parameters
+void	execute_program(char *prog_path, char **env)
+{
+	int	pid;
+	int	exec_ret;
+
+	pid = fork();
+	if (pid == 0)
+	{
+		printf("Child process\n");
+		exec_ret = execve(prog_path, NULL, env);
+		if (exec_ret == -1)
+			program_error();
+	}
+	else
+	{
+		printf("Parent process\n");
+		waitpid(pid, NULL, 0);
+	}
 }

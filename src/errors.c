a9a0d5490f3b313e2ed0991e7004db28a0b61fed
Clément Paluszek <105011036+Cpaluszek@users.noreply.github.com>
2022-12-18 10:52:50 +0100

    norm

--- src/errors.c
+++ src/errors.c
@@ -6,7 +6,7 @@
 /*   By: cpalusze <cpalusze@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/18 09:27:41 by cpalusze          #+#    #+#             */
-/*   Updated: 2022/12/18 10:09:56 by cpalusze         ###   ########.fr       */
+/*   Updated: 2022/12/18 10:52:34 by cpalusze         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,4 +39,3 @@ void	allocation_error(void)
 	ft_printf_fd(STDERR_FILENO, "Allocation error\n");
 	exit(4);
 }
-

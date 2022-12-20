db00abe3b375edab41b75959046109ead775f8f7
Clément Paluszek <105011036+Cpaluszek@users.noreply.github.com>
2022-12-20 09:04:34 +0100

    malloc protection in search_in_paths

--- src/parse_program.c
+++ src/parse_program.c
@@ -6,13 +6,13 @@
 /*   By: cpalusze <cpalusze@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/18 10:43:22 by cpalusze          #+#    #+#             */
-/*   Updated: 2022/12/20 08:57:24 by cpalusze         ###   ########.fr       */
+/*   Updated: 2022/12/20 09:04:13 by cpalusze         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-static char	*search_in_paths(char **paths, char *prog_name);
+static char	*search_in_paths(char **paths, char *prog_name, int *error);
 static char	**access_absolute_path(char **prog_with_args);
 static void	alloc_error_exit(char **split);
 
@@ -20,21 +20,25 @@ static void	alloc_error_exit(char **split);
 // In PATH if no absolute path
 char	**parse_program(char *prog_name, t_pipex *pipex)
 {
-	char	**prog_with_args;
+	char	**prog_args;
 	char	*temp;
+	int		alloc_error;
 
-	prog_with_args = ft_split(prog_name, ' ');
-	if (prog_with_args == NULL)
+	prog_args = ft_split(prog_name, ' ');
+	if (prog_args == NULL)
 		print_error_exit(ALLOC_ERROR);
-	if (ft_strchr(prog_with_args[0], '/') != NULL)
-		return (access_absolute_path(prog_with_args));
-	temp = ft_strjoin("/", prog_with_args[0]);
-	free(prog_with_args[0]);
-	prog_with_args[0] = temp;
-	if (prog_with_args[0] == NULL)
-		alloc_error_exit(prog_with_args);
-	prog_with_args[0] = search_in_paths(pipex->paths, prog_with_args[0]);
-	return (prog_with_args);
+	if (ft_strchr(prog_args[0], '/') != NULL)
+		return (access_absolute_path(prog_args));
+	temp = ft_strjoin("/", prog_args[0]);
+	free(prog_args[0]);
+	prog_args[0] = temp;
+	if (prog_args[0] == NULL)
+		alloc_error_exit(prog_args);
+	alloc_error = 0;
+	prog_args[0] = search_in_paths(pipex->paths, prog_args[0], &alloc_error);
+	if (alloc_error)
+		alloc_error_exit(prog_args);
+	return (prog_args);
 }
 
 // Search for PATH variable in environment
@@ -77,24 +81,27 @@ static char	**access_absolute_path(char **prog_with_args)
 	return (NULL);
 }
 
-// Note: is malloc protection on join needed? or dup?
 // Check if we can find the program using env
 // 		access: F_OK + X_OK => program exist and can be executed
-static char	*search_in_paths(char **paths, char *prog_name)
+static char	*search_in_paths(char **paths, char *prog_name, int *error)
 {
 	int		i;
 	char	*prog_path;
 	char	*join;
 
-	i = 0;
+	i = -1;
 	prog_path = NULL;
-	while (paths[i])
+	while (paths[++i])
 	{
 		join = ft_strjoin(paths[i], prog_name);
+		if (join == NULL)
+		{
+			*error = 1;
+			break ;
+		}
 		if (access(join, F_OK | X_OK) == 0)
 			prog_path = ft_strdup(join);
 		free(join);
-		i++;
 	}
 	if (prog_path == NULL)
 	{
